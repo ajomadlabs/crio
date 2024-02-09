1d06656fe55cb77a4dba990121289d480f26a190
ajomadlabs <ajojohn555@gmail.com>
2024-02-09 19:17:48 +0530

    fix: session counter basic

---
+++ src/SessionOne/Counter.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+  increment() {
+    this.setState((prevState) => ({ count: prevState.count + 1 }));
+  }
+  decrement() {
+    this.setState((prevState) => ({ count: prevState.count - 1 }));
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={() => this.increment()}>+</button>
+        <span>{this.state.count}</span>
+        <button onClick={() => this.decrement()}>-</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
