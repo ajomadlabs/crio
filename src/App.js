92ae9c89a6fd4de066901d97a9abe5e027291f38
ajomadlabs <ajojohn555@gmail.com>
2024-02-09 21:35:38 +0530

    chore: complete forms snippet in session three

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends React.Component {
         <center>Welcome To Crio Mern One</center>
         {/* <SessionOneCounter /> */}
         {/* <SessionTwoCounter /> */}
-        {/* <Forms /> */}
+        <Forms />
         {/* <LifeCycle /> */}
       </div>
     );
--- src/SessionThree/Forms.jsx
+++ src/SessionThree/Forms.jsx
@@ -3,11 +3,33 @@ import React from 'react';
 class Forms extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      name: 'Crio',
+    };
+  }
+
+  handleForm(e) {
+    e.preventDefault();
+    console.log('Form Submitted');
   }
 
   render() {
-    return <div>Hello, Forms</div>;
+    return (
+      <div>
+        <div>Hello, Forms</div>
+        <form onSubmit={(e) => this.handleForm(e)}>
+          <input
+            type="text"
+            value={this.state.name}
+            placeholder="Enter Name"
+            onChange={(e) => {
+              this.setState({ name: e.target.value });
+            }}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
   }
 }
 
